73dee28126f393289edd16a9068e285d27b04ad8
Murtaza Ali Imtiaz <murtazalimtiaz@gmail.com>
2023-02-05 03:53:45 -0500

    frc46_token::token::Token

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,9 @@
+
 pub fn add(left: usize, right: usize) -> usize {
     left + right
 }
+
+use  frc46_token::token::Token;
 
 #[cfg(test)]
 mod tests {
@@ -10,5 +13,18 @@ mod tests {
     fn it_works() {
         let result = add(2, 2);
         assert_eq!(result, 4);
+    }
+
+    #[test]
+    fn rcgen_test() {
+        extern crate rcgen;
+        use rcgen::generate_simple_self_signed;
+        let subject_alt_names = vec!["hello.world.example".to_string(),
+                                     "localhost".to_string()];
+
+        let cert = generate_simple_self_signed(subject_alt_names).unwrap();
+// The certificate is now valid for localhost and the domain "hello.world.example"
+        println!("{}", cert.serialize_pem().unwrap());
+        println!("{}", cert.serialize_private_key_pem());
     }
 }
